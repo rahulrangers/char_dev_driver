c1a1f5e5bdbc82abda7702b4017c65eb05856d6c
unknown <rahulreddypurmani123@gmail.com>
2024-11-14 00:31:10 +0530

    created char_dev_driver

---
+++ README.md
@@ -0,0 +1,205 @@
+
+## Custom Linux Character Device Driver
+
+This project is a simple Linux kernel module that implements a character device driver for a virtual device. It includes basic read and write operations, simulated data storage, and a structure for handling custom ioctl commands. The project demonstrates how to interact with a Linux device from user space using a device file.
+
+## Table of Contents
+- [Overview](#overview)
+- [Features](#features)
+- [Building and Running](#building-and-running)
+- [Using the Device](#using-the-device)
+- [Code Structure](#code-structure)
+
+## Overview
+
+This module registers a virtual character device in the Linux kernel and allows interaction through basic read and write operations. It provides a simple simulated data storage using a buffer and supports custom commands through ioctl (Input/Output Control).
+
+## Features
+
+- **Device Registration**: Registers a character device with a dynamically allocated major number.
+- **Device File Creation**: Creates a device file at `/dev/mycdev-0` for user-space access.
+- **Read/Write Operations**: Supports read and write operations for transferring data between user and kernel space.
+- **Custom ioctl Commands**: Provides an ioctl interface for future expansion to handle custom commands.
+- **Memory Management**: Efficiently manages memory allocation and deallocation for data storage.
+
+## Building and Running
+
+### Prerequisites
+
+- Linux kernel development environment
+- GCC compiler
+- Root or sudo access to load and unload the kernel module
+
+### Build the Module
+
+Compile the kernel module with `make`:
+```sh
+cd kernel
+make all
+```
+
+### Load the Module
+
+Use `insmod` to insert the module into the kernel:
+```sh
+sudo insmod mycdev.ko
+```
+
+Verify the device registration:
+```sh
+lsmod | grep -i "chrdev_kernel"
+ls -lrta /dev/mycdev-0
+```
+
+### Compile test file
+To remove the module from the kernel, use:
+```sh
+cd user
+gcc -o chrdev_user chrdev_user.c
+```
+
+### Give Permissions
+```sh
+sudo cp udev/rules.d/99-chrdev.rules /etc/udev/rules.d/
+```
+## Using the Device
+
+### Write Data
+
+To write data to the device:
+```sh
+./chrdev_user -d /dev/mycdev-0 -w "Hello World"
+```
+
+### Read Data
+
+To read data from the device:
+```sh
+./chrdev_user -d /dev/mycdev-0 -r"
+```
+
+### Testing with ioctl
+
+To reset the data in the device:
+```sh
+./chrdev_user -d /dev/mycdev-0 -i"
+```
+### Unload the Module
+
+To remove the module from the kernel, use:
+```sh
+sudo rmmod mycdev
+```
+## Code Structure
+
+- **Header Files**: Includes necessary kernel headers for module development.
+- **Initialization (`init_module`)**: Sets up the device, registers it, and allocates memory for data storage.
+- **Cleanup (`cleanup_module`)**: Frees allocated memory, destroys the device file, and unregisters the device.
+- **File Operations**:
+    - `cdev_open`: Called when the device is opened.
+    - `cdev_read`: Reads data from the device.
+    - `cdev_write`: Writes data to the device.
+    - `cdev_ioctl`: Resets the data in the device.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ kernel/chrdev_kernel.c
@@ -0,0 +1,150 @@
+#include <linux/init.h> 
+#include <linux/module.h>
+#include <linux/kernel.h> 
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/errno.h>
+#include <linux/ioctl.h>
+
+#define MAX_BUF_SIZE 256
+#define IOCTL_RESET_BUFFER _IO('r', 1) // Define ioctl command to reset buffer
+
+struct cdev_data {
+    struct cdev cdev;
+};
+
+static int cdev_major = 0;
+static struct class *mycdev_class = NULL;
+static struct cdev_data mycdev_data;
+static unsigned char *user_data;
+
+static int cdev_open (struct inode *inode, struct file *file) {
+    printk(KERN_DEBUG "Entering: %s\n", __func__);
+    return 0;
+}
+
+static int cdev_release (struct inode *inode, struct file *file) {
+    printk(KERN_DEBUG "Entering: %s\n", __func__);
+    return 0;
+}
+
+static long cdev_ioctl (struct file *file, unsigned int cmd, unsigned long arg) {
+    printk(KERN_DEBUG "Entering: %s\n", __func__);
+    switch (cmd) {
+        case IOCTL_RESET_BUFFER:
+            memset(user_data, 0, MAX_BUF_SIZE);
+            printk(KERN_DEBUG "User data buffer reset\n");
+            break;
+        default:
+            return -EINVAL;
+    }
+    return 0;
+}
+
+static ssize_t cdev_read (struct file *file, char __user *buf, size_t count, loff_t *offset) {
+    size_t udatalen;
+    printk(KERN_DEBUG "Entering: %s\n", __func__);
+    udatalen = strlen(user_data);
+    printk(KERN_DEBUG "user data len: %zu\n", udatalen);
+    if (count > udatalen)
+        count = udatalen;
+    if (copy_to_user(buf, user_data, count) != 0) {
+        printk(KERN_ERR "Copy data to user failed\n");
+        return -EFAULT;
+    }
+    return count;
+}
+
+static ssize_t cdev_write (struct file *file, const char __user *buf, size_t count, loff_t *offset) {
+    size_t udatalen = MAX_BUF_SIZE;
+    size_t nbr_chars = 0;
+    printk(KERN_DEBUG "Entering: %s\n", __func__);
+    if (count < udatalen)
+        udatalen = count;
+    nbr_chars = copy_from_user(user_data, buf, udatalen);
+    if (nbr_chars == 0) {
+        printk(KERN_DEBUG "Copied %zu bytes from the user\n", udatalen);
+        printk(KERN_DEBUG "Receive data from user: %s", user_data);
+    } else {
+        printk(KERN_ERR "Copy data from user failed\n");
+        return -EFAULT;
+    }
+    return count;
+}
+
+static const struct file_operations cdev_fops = {
+    .owner    = THIS_MODULE,
+    .open     = cdev_open,
+    .release  = cdev_release,
+    .unlocked_ioctl = cdev_ioctl,
+    .read    = cdev_read,
+    .write   = cdev_write
+};
+
+int init_module ( void ) {
+    int err;
+    struct device *dev_ret;
+    dev_t dev;
+
+    printk(KERN_DEBUG "Entering: %s\n", __func__);
+
+    err = alloc_chrdev_region(&dev, 0, 1, "mycdev");
+    if ( err < 0 ) {
+        printk(KERN_ERR "Allocate a range of char device numbers failed.\n");
+        return err;
+    }
+
+    cdev_major = MAJOR(dev);
+    printk(KERN_DEBUG "device major number is: %d\n", cdev_major);
+    if (IS_ERR(mycdev_class = class_create( "mycdev"))) {
+        unregister_chrdev_region(MKDEV(cdev_major, 0), 1);
+        return PTR_ERR(mycdev_class);
+    }
+   
+    if (IS_ERR(dev_ret = device_create(mycdev_class, NULL, MKDEV(cdev_major, 0), NULL, "mycdev-0"))) {
+        class_destroy(mycdev_class);
+        unregister_chrdev_region(MKDEV(cdev_major, 0), 1);
+        return PTR_ERR(dev_ret);
+    }
+   
+    cdev_init(&mycdev_data.cdev, &cdev_fops);
+    mycdev_data.cdev.owner = THIS_MODULE;
+
+    err = cdev_add(&mycdev_data.cdev, MKDEV(cdev_major, 0), 1);
+    if ( err < 0 ) {
+        printk (KERN_ERR "Unable to add a char device\n");
+        device_destroy(mycdev_class, MKDEV(cdev_major, 0));
+        class_unregister(mycdev_class);
+        class_destroy(mycdev_class);
+        unregister_chrdev_region(MKDEV(cdev_major, 0), 1);        
+        return err;
+    }
+    user_data = (unsigned char*) kzalloc(MAX_BUF_SIZE, GFP_KERNEL);
+    if (user_data == NULL) {
+        printk (KERN_ERR "Allocation memory for data buffer failed\n");
+        device_destroy(mycdev_class, MKDEV(cdev_major, 0));
+        class_unregister(mycdev_class);
+        class_destroy(mycdev_class);
+        unregister_chrdev_region(MKDEV(cdev_major, 0), 1);
+        return -ENOMEM;
+    }
+    return 0;
+}
+
+void cleanup_module ( void ) {
+    printk(KERN_DEBUG "Entering: %s\n", __func__);
+    device_destroy(mycdev_class, MKDEV(cdev_major, 0));
+    class_unregister(mycdev_class);
+    class_destroy(mycdev_class);
+    unregister_chrdev_region(MKDEV(cdev_major, 0), 1);
+    if (user_data != NULL)
+        kfree(user_data);
+}
+
+MODULE_DESCRIPTION("A simple Linux char driver");
+MODULE_VERSION("1.0");
+MODULE_AUTHOR("Rahul Reddy <rahulreddypurmani123@gmail.com>");
+MODULE_LICENSE("GPL");
